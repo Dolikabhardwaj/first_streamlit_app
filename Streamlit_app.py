a955e5a162d2842be712b68f34bd31f2dfe27f11
Dolikabhardwaj <148039843+Dolikabhardwaj@users.noreply.github.com>
2023-10-26 13:17:48 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,6 @@
 import Streamlit
 Streamlit.title('My Parents Little Healthy Dinner')
+Streamlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 and Blueberry oatmeal')
+Streamlit.text('kale,Spinach and Smoothie')
+Streamlit.text('Hard-Boiled Free-Range Egg')
