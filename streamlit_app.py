4c0493d8f4157eae8d81409f2fcaafd9da50fb7b
Dolikabhardwaj <148039843+Dolikabhardwaj@users.noreply.github.com>
2023-10-28 13:46:13 +0530

    Update and rename Streamlit_app.py to streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit
+streamlit.title('My Parents Little Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 and Blueberry oatmeal')
+streamlit.text(' 🥗 kale,Spinach and Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas as pd
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
